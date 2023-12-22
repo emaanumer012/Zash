f68f572a393523bf2e96605bc472207ca6b63192
hifahd <fahd-ahmad@outlook.com>
2023-12-22 11:09:00 +0500

    Added remaining functions in CartController

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -16,3 +16,29 @@ class CartController extends Controller
 
         return view("cart",["items"=>$items]);
     }
+    public function AddToCart()
+    {
+        $product = new Cart();
+        $product->productID = request('productID');
+        $product->cart_id = 1;
+        $product->Quantity = 1;
+        $product->save();
+        
+        return redirect('/homepage');
+    }
+    public function UpdateCart()
+    {
+        $id= request("productID");
+        $q=(int)request("quantity");
+        $UpdateDetails = Cart::where('productID', '=',  $id)->first();
+        $UpdateDetails->Quantity += $q;
+        $UpdateDetails->save();
+        if ($q == 0)
+        {
+            DB::table('carts')->where('productID', $id)->delete();
+        }
+        DB::table('carts')->delete($id);
+        return redirect('/cart');
+    }
+
+}
